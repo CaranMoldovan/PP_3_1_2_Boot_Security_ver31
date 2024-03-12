42ba6502760036dff175dc380ffab21f07e8d53a
CaranMoldovan <116651970+CaranMoldovan@users.noreply.github.com>
2024-03-12 20:35:26 +0300

    first commit

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -51,8 +51,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/static/css/**");
-    }
 }
